e99509445fd56b357358b8fe1962bbf31592d711
DUrttu <48917309+DUrttu@users.noreply.github.com>
2019-03-30 17:29:08 +0200

    Update ruoka.js

--- ruoka.js
+++ ruoka.js
@@ -19,7 +19,7 @@ function Ruoka() {
   var j = Math.floor(Math.random() * 15) * grid + 76;
 
   this.show = function() {
-    //fill(200, 0, 0);
+    fill(200, 0, 0);
     //rect(f, j, grid, grid,);
     image(omena, f, j, grid, grid);
   }
